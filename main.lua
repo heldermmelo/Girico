4633258c2e8d1d67a3882d2fe5dff90842e6cc6c
Helder de Medeiros Melo <heldermmelo@gmail.com>
2015-03-16 11:09:09 -0300

    GiricoRoadRage
    Imagem do carro adicionada. Movimento imperfeito

--- main.lua
+++ main.lua
@@ -7,11 +7,19 @@
 -- Your code here
 --Oulta a barra de status desde o começo
 display.setStatusBar( display.HiddenStatusBar )
+--inicializa o módulo de física
+physics =require("physics")
+
+physics.start()
+physics.setDrawMode("hybrid")
  
--- Inicializa variáveis de exibição da imagem de fundo em duas variáveis
+-- Inicializa variáveis de exibição da imagem de fundo em duas variáveis. Inicializa variáveis de movimento do carro
 _W = display.contentWidth; -- Get the width of the screen
 _H = display.contentHeight; -- Get the height of the screen
+
 scrollSpeed = 6; -- Set Scroll Speed of background
+speed=0
+movimentox=0;
  
 -- Adiciona primeiro background!
 local bg1 = display.newImageRect("estradaImagem.png", 320, 480)
@@ -42,7 +50,7 @@ bg3.y = bg3.y + scrollSpeed
 bg4.y = bg4.y + scrollSpeed
  
 -- Estabelece listeners para perceber quando a imagem vai acabar
--- e mover o fundo de novo.
+-- e mover o background para o começo da tela.
 if (bg1.y + bg1.contentHeight) >480*4 then
 bg1:translate( 0, -480 )
 end
@@ -55,7 +63,46 @@ end
 if (bg4.y +  bg4.contentHeight) > 480 then
 bg4:translate( 0, -480)
 end
-end
+local carroHeroi=display.newImage("recursos/carrinho.png", 160,240)
+--physics.addBody( carroHeroi, "static", { friction=0, bounce=0 })
+-- Adiciona botão esquerdo
+ local left = display.newImage ("recursos/btn_arrow.jpg")
+ left.x = 45; left.y = 475;
+ left:scale(0.2,0.2)
+ left.rotation = 180;
+-- Adiciona botão direito
+ local right = display.newImage ("recursos/btn_arrow.jpg")
+ right.x = 120; right.y = 475;
+ right:scale(0.2, 0.2)
+ -- When left arrow is touched, move character left
+ function left:touch()
+ movimentox = speed-0.2;
  
+ end
+ left:addEventListener("touch",left)
+-- When right arrow is touched, move character right
+ function right:touch()
+ movimentox = speed+0.2;
+ end
+ right:addEventListener("touch",right)
+ 
+ -- Move carro à direita
+ local function moveCarro (event)
+ carroHeroi.x = carroHeroi.x + movimentox;
+
+ end
+  Runtime:addEventListener("enterFrame", moveCarro)
+ 
+ 
+
+end
+--para o jogador quando nenhum botão estiver sendo apertado!
+local function stop (event)
+ if event.phase =="ended" then
+ movimentox = 0;
+ end
+ end
+ Runtime:addEventListener("touch", stop )
+  
 -- Create a runtime event to move backgrounds
 Runtime:addEventListener( "enterFrame", move )
